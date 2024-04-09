64d5f3151c940cb2c9bcfb5a469f961ced3151e7
Thanh Dat <tthanhdat.pb@gmail.com>
2024-04-09 22:43:54 +0700

    update 090424 v2

--- src/main/java/com/rs/retailstore/config/DataSourceConfig.java
+++ src/main/java/com/rs/retailstore/config/DataSourceConfig.java
@@ -1,6 +1,5 @@
 package com.rs.retailstore.config;
 
-//import org.springframework.context.annotation.Bean;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
